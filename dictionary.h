2d28bc471f291d35525a3d3026da15a90b06797d
Angie Pinilla <angelinepinilla@gmail.com>
2016-05-07 18:21:40 -0400

    header file for vocab storage and future hashing

---
+++ dictionary.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+	char *word;
+	int count;
+	struct node *next;
+};
+
+struct hash_table{
+	int size;
+	node **chain;
+};
+
+struct node *new_node(char *key);
+void new_hashtable(struct hash_table *table, int M);
+int update_chain(struct node *n, char *key);
+int hash(char *key, int M);
+
